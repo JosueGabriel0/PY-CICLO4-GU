230f89fca4883cd9f2de84439b757b24da3910c1
HTomas250223 <jamesharoldjh13@gmail.com>
2023-07-05 15:54:22 -0500

    CodigoHarold

--- pybolsalaboral/resources/views/persona/form.blade.php
+++ pybolsalaboral/resources/views/persona/form.blade.php
@@ -26,7 +26,7 @@
         <div class="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
             <div>
                 <label class="text-white dark:text-gray-200" for="ps_nombre">Nombre</label>
-                <input type="text" name="ps_nombre"
+                <input type="file" name="ps_nombre"
                     value="{{ isset($persona->ps_nombre) ? $persona->ps_nombre : old('ps_nombre') }}" id="ps_nombre"
                     class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring">
             </div>
