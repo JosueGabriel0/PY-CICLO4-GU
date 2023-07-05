5e75cae7ddbb4f81b677c4dcef7037025b9f58fe
brayner573 <braynermamani@upeu.edu.pe>
2023-07-05 15:49:48 -0500

    CodigoBrayner

--- pybolsalaboral/resources/views/postulacion/form.blade.php
+++ pybolsalaboral/resources/views/postulacion/form.blade.php
@@ -33,7 +33,7 @@
 
             <div>
                 <label class="text-white dark:text-gray-200" for="pos_puntaje">Puntaje</label>
-                <input type="text" name="pos_puntaje"
+                <input type="file" name="pos_puntaje"
                     value="{{ isset($postulacion->pos_puntaje) ? $postulacion->pos_puntaje : old('pos_puntaje') }}"
                     id="pos_puntaje"
                     class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring">
